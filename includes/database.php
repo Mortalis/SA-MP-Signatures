6d7387f12b335667ddb088637eacb189aa930aa8
Djole1337 <pop96x@gmail.com>
2013-12-20 18:02:04 +0100

    ok

---
+++ includes/database.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @project:
+ *      SA:MP Dynamic Signatures
+ * @file:
+ *		database.php
+ * @author:
+ *      Djole
+ * @version:
+ *      v1.0
+ * @last updated:
+ *      N/A
+ */
+
+class DB extends PDO
+{
+	protected static
+		$iInstance = NULL;
+	
+	public static function getInstance()
+	{
+		if (self::$iInstance === NULL)
+		{
+			global
+				$aGlobalConfig;
+
+			self::$iInstance = new DB
+			(
+				"mysql:host={$aGlobalConfig["mysql"]["host"]};port={$aGlobalConfig["mysql"]["port"]};dbname={$aGlobalConfig["mysql"]["name"]};",
+				$aGlobalConfig["mysql"]["user"],
+				$aGlobalConfig["mysql"]["pass"]
+			);
+		}
+		return self::$iInstance;
+	}
+}
